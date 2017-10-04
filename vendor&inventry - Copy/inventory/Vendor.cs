f6919b76b3165d027d3bc0d0c795915aceb01a28
surathruwan <surathruwan@gmail.com>
2017-10-04 14:19:08 +0530

    Hii

--- vendor&inventry - Copy/inventory/Vendor.cs
+++ vendor&inventry - Copy/inventory/Vendor.cs
@@ -18,6 +18,7 @@ namespace vendor_management
     public partial class Vendor : Form
     {
         //made a change
+        //Hiiiii
 
         //SSlmode =none because it gave an error in connecting
         string constring = "Sslmode=none;datasource=localhost;port=3306;username=root;password=  ";
